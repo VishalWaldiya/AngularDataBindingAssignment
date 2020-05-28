0c5a738002d43471e94687d3534b4026137d4e51
vishalwaldiya <vishalwaldiya@gmail.com>
2020-05-28 16:39:44 +0530

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DataBindingAssignment';
+  username="";
+
+  isUsername(){
+    if (this.username.length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  reset(){
+    this.username="";
+  }
 }
